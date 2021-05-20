7602754c07d94ae37ced4d78625e2fa1c2aca475
13Diaz <jmdiaz0911@misena.edu.co>
2021-05-20 16:00:28 -0500

    Se corrigen los errores del backend al ingresar los puntos

--- torneo/interfazSet/puntos/puntosSet.php
+++ torneo/interfazSet/puntos/puntosSet.php
@@ -55,11 +55,9 @@
           </a>
         </div>
         <div class="col-auto">
+        
           <!--    FORMULARIO PARA ENVIAR LOS PUNTOS Y AMONESTACIONES A LA BASE DE DATOS   -->
           <form action="recibirPuntos.php" method="POST">
-          <input type="hidden" name="codigoEncuentro" value="<?php $codigoEncuentro; ?>">
-          <input type="hidden" name="codigoEquipo1" value="<?php $codigoEquipo1; ?>">
-          <input type="hidden" name="codigoEquipo2" value="<?php $codigoEquipo2; ?>">
           <button class="btn button" name="enviarPuntos" type="submit">
             <b>Guardar</b>
           </button>
@@ -97,15 +95,12 @@
             $mostrar = mysqli_fetch_assoc($consulta);
             $codigoSet = $mostrar['mayorCodigoSet'];
             $numeroRegistro = $mostrar['mayorNumeroRegistro'];
-
-            if ($codigoSet == 0 || $codigoSet == null){
-              $sql = "INSERT INTO zet (NumeroRegistro, Cod_Set, Cod_Encuentro)
-                      VALUES ( $numeroRegistro + 1, $codigoSet + 1, $codigoEncuentro )";
-              $consulta = mysqli_query($conexion,$sql);
-              echo $codigoSet + 1;
-            }else{
-              echo $codigoSet;
-            }
+            $codigoSet ++;
+            if ($codigoSet <= 3){
+            echo $codigoSet;            
+            $sql = "INSERT INTO zet (NumeroRegistro, Cod_Set, Cod_Encuentro)
+                    VALUES ( $numeroRegistro + 1, $codigoSet, $codigoEncuentro )";
+            $consulta = mysqli_query($conexion,$sql);
           ?>
         </h2>
       </center>
@@ -201,10 +196,26 @@
         </section>
       </div>
     </main>
-    <input type="hidden" name="codigoSet" value="<?php $codigoSet; ?>">
+    <input type="hidden" name="numeroRegistro" value="<?php echo $numeroRegistro; ?>">
+    <input type="hidden" name="codigoSet" value="<?php echo $codigoSet; ?>">
+    <input type="hidden" name="codigoEncuentro" value="<?php echo $codigoEncuentro; ?>">
+    <input type="hidden" name="codigoEquipo1" value="<?php echo $codigoEquipo1; ?>">
+    <input type="hidden" name="codigoEquipo2" value="<?php echo $codigoEquipo2; ?>">
     <input type="hidden" name="puntosEquipo1" id="puntosEquipo1" value="0">
     <input type="hidden" name="puntosEquipo2" id="puntosEquipo2" value="0">
     </form>
+    <?php
+            }else{
+              ?>
+              <center><h3><b>Este encuentro ya ha sido disputado los resultados podra
+                              encontrarlos en la seccion de resultados</b></h3>
+                              <p>
+                              presione la flecha que esta en la parte superior izquierda para 
+                              escoger un encuentro distinto
+                              </p></center>
+              <?php
+            }
+    ?>
 
     <!-- Scripts de bootstrap-->
     <script
